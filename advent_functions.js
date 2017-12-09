3a23a2882dcb319b5d06863b9226eed8c42a6c92
Lawrence Getubig <lawrencegetubig@lars-larsonson.home>
2017-12-09 18:16:59 -0500

    fix js bugs, click on image changes kid, click on button xmasify, add new styles for new elements

--- advent_functions.js
+++ advent_functions.js
@@ -2,18 +2,22 @@
 
 // xmasify function
 function xmasify() {
-
-}
+	// find target h1 greeting
+	var targetH1 = $('body').find('h1');
+	var targetImg = $('body').find('img#hat')
+	targetH1.toggleClass('visible');
+	targetImg.toggleClass('visible');
+	var targetBody = $('body').toggleClass('xmasify');
+}// end xmasify
 
 
 // changeKid function triggered by double click
 function changeKid() {
-	console.log("change kid active with double click")
 	//get target img
 	var targetImg = $('body').find('img.kid-image');
 	// get id of targetImg
 	var targetImgId = targetImg.attr('id');
-	alert(targetImgId);
+	// if img id is ella, change to maco, if maco change to mio, else change to ella
 	if (targetImgId === "ella") {
 		targetImg.attr('src', 'images/maco_advent_image.jpg');
 		targetImg.attr('id', 'maco');
@@ -24,14 +28,14 @@ function changeKid() {
 		targetImg.attr('src', 'images/ella_advent_image.jpg');
 		targetImg.attr('id', 'ella');
 	}
-}
+}// end changeKid
 
 
 /*event listeners*/
 
 // code that listens to user click, when user clicks, make hat + greeting visible
-$('body').on('click', xmasify);
+$('button.xmasify').on('click', xmasify);
 
 
 // code that listens to double click, when user double clicks, change kid image
-$('body').on('dblclick', changeKid);
+$('img').on('click', changeKid);
